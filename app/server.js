dd924c1b9b48d144586186758c808fe0e2918057
Allan Pearson <Alprsn2012@gmail.com>
2019-09-21 11:44:30 -0500

    line coded added

--- app/server.js
+++ app/server.js
@@ -1,6 +1,8 @@
 
 ///server listens for what to require and make the call for us.
-
+var express = require("express");
+var app = express(); 
+var PORT = process.eventNames.PORT || 3306;
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
