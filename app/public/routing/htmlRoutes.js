78c3000349a32271a0766d9df8adfa104dab06a3
Allan Pearson <Alprsn2012@gmail.com>
2019-09-21 01:01:38 -0500

    line code added to survey.html file

--- app/public/routing/htmlRoutes.js
+++ app/public/routing/htmlRoutes.js
@@ -1,7 +1,7 @@
 var path = require("path");
 // This routing is for survey and home.html to get the paths made where.
 module.exports = function(app) {
-    app.get("/friendsfill", function(req, res) {
+    app.get("/friendFill", function(req, res) {
         res.sendFile(path.join(_dirname, "../public/survey.html"));
         });
            app.get("*", function(req, res) {
