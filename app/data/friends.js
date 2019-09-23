7974f313562d8f7f8881bb9b9d0a0604d456c64f
Allan Pearson <Alprsn2012@gmail.com>
2019-09-23 14:06:07 -0500

    line code added loines 175-177

---
+++ app/data/friends.js
@@ -0,0 +1,28 @@
+DROP DATABASE IF EXISTS friendsDB;
+
+CREATE DATABASE friendsDB;
+
+USE friendsDB;
+
+CREATE TABLE  newfriend (
+ id  INT NOT NULL AUTO_INCREMENT,
+ name VARCHAR(30) NOT NULL,
+  title VARCHAR(60) NOT NULL,
+  image VARCHAR(50) NOT NULL,
+  Q1 VARCHAR(20) NOT NULL,
+  Q2 VARCHAR(20) NOT NULL,
+  Q3 VARCHAR(20) NOT NULL,
+  Q4 VARCHAR(20) NOT NULL,
+  Q5 VARCHAR(20) NOT NULL,
+  Q6 VARCHAR(20) NOT NULL,
+  Q7 VARCHAR(20) NOT NULL,
+  Q8 VARCHAR(20) NOT NULL,
+  Q9 VARCHAR(20) NOT NULL,
+  Q10 VARCHAR(20) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+INSERT INTO newfriend(name, title, image, Q1, Q2, Q3, Q4, Q5, Q6, Q7, Q8, Q9, Q10)
+VALUES ("name", "title", "image", "Q1", "Q2", "Q3", "Q4", "Q5", "Q6", "Q7", "Q8", "Q9", "Q10");
+
+SELECT * from newfriend; 
