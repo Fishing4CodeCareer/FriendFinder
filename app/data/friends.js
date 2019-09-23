f7431faab5d6a74950138c0878c468c71503f04f
Allan Pearson <Alprsn2012@gmail.com>
2019-09-23 17:30:25 -0500

    friends.js line coded added to data lines 1-17

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+module.exports = friendsArray;
+
+var friendsArray = [
+  {
+  title: "name",
+  image: "image",
+  Q1: "Q1",
+  Q2: "Q2",
+  Q3: "Q3",
+  Q4: "Q4",
+  Q5: "Q5",
+  Q6: "Q6",
+  Q7: "Q7",
+  Q8: "Q8",
+  Q9: "Q9",
+  Q10: "Q10",
+}];
+
+//THis will load the the array module to allow use to other files that are needed.
+
