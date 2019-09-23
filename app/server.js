03e19ccb9dc265deb69e08f9a269abd21a990c5c
Allan Pearson <Alprsn2012@gmail.com>
2019-09-23 15:04:22 -0500

    line code corrections

--- app/server.js
+++ app/server.js
@@ -1,11 +1,12 @@
-
-///server listens for what to require and make the call for us.
+//server listens for what to require and make the call for us.
 var express = require("express");
 var app = express(); 
-var PORT = process.eventNames.PORT || 3306;
+var PORT = process.env.PORT || 3306;
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
-
 
 // Starts server for me.
 app.listen(PORT, function() {console.log("App listening on PORT: " + PORT);
