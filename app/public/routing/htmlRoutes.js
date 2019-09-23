cc61d2258864ac288d4afd5c6529d6758e46d75e
Allan Pearson <Alprsn2012@gmail.com>
2019-09-23 18:28:22 -0500

    line code deletion line 5 / 8 _dirname, removed

--- app/public/routing/htmlRoutes.js
+++ app/public/routing/htmlRoutes.js
@@ -2,9 +2,9 @@ var path = require("path");
 // This routing is for survey and home.html to get the paths made where.
 module.exports = function(app) {
     app.get("/friends", function(req, res) {
-        res.sendFile(path.join(_dirname, "../public/survey.html"));
+        res.sendFile(path.join("../public/survey.html"));
         });
            app.get("*", function(req, res) {
-                 res.sendFile(path.join(_dirname,  "../public/home.html"));
+                 res.sendFile(path.join("../public/home.html"));
              });
 };
