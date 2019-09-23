e6918ed81c1a595671cf023626039a82b3717b20
Allan Pearson <Alprsn2012@gmail.com>
2019-09-23 15:16:21 -0500

    line code added

--- app/public/routing/apiRoutes.js
+++ app/public/routing/apiRoutes.js
@@ -1,30 +1,30 @@
-var reserveData = require("../data/reserveData");
-var newfriendData = require("../data/friendShowData");
+var friendsData = require("../data/friendsData");
+var friendsData = require("../data/friendsData");
 //API take the request from the form fill data and the friends form.
 // The data is then pulled back down from the server to post the form filled results.
 module.exports = function(app) {
-    app.get("/api/reserve", function(req, res){
+    app.get("/api/friends", function(req, res){
 
         res.json(friendsData);
     });
 }
-    app.post("/api/reserve", function(req, res){
-        res.json(newfriendData);
+    app.post("/api/friends", function(req, res){
+        res.json(friendsData);
         });
 
-        app.post("/api/reserve", function(req, res) {
-            if (newfriendData.length <10) {
-            newfriendData.push(req.body);
+        app.post("/api/friends", function(req, res) {
+            if (friendsData.length <10) {
+            friendsData.push(req.body);
             res.json(true);
             }
             else {
-                newfriendData.push(req.body);
+                friendsData.push(req.body);
                 res.json(false);
             }
         });
         // Below is api call to clear form data.
         app.post("/api/clear", function(req,res) {
             reserve.length = 0;
-            newfriendData.length = 0;
+            friendsData.length = 0;
             res.json({ ok: true });
         });
